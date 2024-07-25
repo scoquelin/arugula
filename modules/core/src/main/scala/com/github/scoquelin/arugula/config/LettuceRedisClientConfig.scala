8001e171345a049b71303c1ef28dd2b19c94e60b
John Loehrer <john.loehrer@meettally.com>
2024-07-25 14:31:18 -0700

    reduce dependencies in core to just lettuce

--- modules/core/src/main/scala/com/github/scoquelin/arugula/config/LettuceRedisClientConfig.scala
+++ modules/core/src/main/scala/com/github/scoquelin/arugula/config/LettuceRedisClientConfig.scala
@@ -1,15 +1,7 @@
 package com.github.scoquelin.arugula.config
 
-import com.typesafe.config.Config
 import io.lettuce.core.resource.{ClientResources => JClientResources}
 
 case class LettuceRedisClientConfig(host: String, port: Int, clientResources: JClientResources = io.lettuce.core.resource.DefaultClientResources.create())
 
-object LettuceRedisClientConfig {
-  def fromConfig(config: Config): LettuceRedisClientConfig = {
-    LettuceRedisClientConfig(
-      host = config.getString("host"),
-      port = config.getInt("port")
-    )
-  }
-}
+
