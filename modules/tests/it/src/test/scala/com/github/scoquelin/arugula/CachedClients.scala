88adfaeaaac483d7ff936422f0336fc06aff36a0
Sebastien Coquelin <seb.coquelin@gmail.com>
2024-07-29 18:22:46 -0700

    Add support for testing Redis operations with other RedisCodec

---
+++ modules/tests/it/src/test/scala/com/github/scoquelin/arugula/CachedClients.scala
@@ -0,0 +1,49 @@
+package com.github.scoquelin.arugula
+
+import com.github.scoquelin.arugula.codec.RedisCodec
+import com.github.scoquelin.arugula.codec.LongCodec
+import com.github.scoquelin.arugula.config.LettuceRedisClientConfig
+import io.lettuce.core.codec.{StringCodec => JStringCodec, RedisCodec => JRedisCodec}
+
+import scala.concurrent.ExecutionContext
+
+sealed trait RedisConnectionType
+
+case object SingleNode extends RedisConnectionType
+
+case object Cluster extends RedisConnectionType
+
+trait CachedClients {
+  def getClient[K, V](codec: RedisCodec[K, V], connectionType: RedisConnectionType): RedisCommandsClient[K, V]
+}
+
+private class RedisCommandCachedClients(redisSingleNodeClientWithStringValue: RedisCommandsClient[String, String],
+                                        redisSingleNodeClientWithLongValue: RedisCommandsClient[String, Long],
+                                        redisClusterClientWithStringValue: RedisCommandsClient[String, String],
+                                        redisClusterClientWithLongValue: RedisCommandsClient[String, Long]) extends CachedClients {
+
+  def getClient[K, V](codec: RedisCodec[K, V], connectionType: RedisConnectionType): RedisCommandsClient[K, V] = {
+    (codec, connectionType) match {
+      case (RedisCodec(JStringCodec.UTF8), SingleNode) => redisSingleNodeClientWithStringValue.asInstanceOf[RedisCommandsClient[K, V]]
+      case (RedisCodec(JStringCodec.UTF8), Cluster) => redisClusterClientWithStringValue.asInstanceOf[RedisCommandsClient[K, V]]
+      case (RedisCodec(LongCodec), SingleNode) => redisSingleNodeClientWithLongValue.asInstanceOf[RedisCommandsClient[K, V]]
+      case (RedisCodec(LongCodec), Cluster) => redisClusterClientWithLongValue.asInstanceOf[RedisCommandsClient[K, V]]
+      case (codec, connectionType) => throw new IllegalStateException(s"Codec $codec not supported for connection type $connectionType")
+    }
+  }
+}
+
+object RedisCommandCachedClients {
+  def apply(singleNodeConfig: LettuceRedisClientConfig, clusterConfig: LettuceRedisClientConfig)(implicit ec: ExecutionContext): CachedClients = {
+    val redisSingleNodeClientWithStringValue = LettuceRedisCommandsClient(singleNodeConfig, RedisCodec.Utf8WithValueAsStringCodec)
+    val redisSingleNodeClientWithLongValue = LettuceRedisCommandsClient(singleNodeConfig, RedisCodec.Utf8WithValueAsLongCodec)
+    val redisClusterClientWithStringValue = LettuceRedisCommandsClient(clusterConfig, RedisCodec.Utf8WithValueAsStringCodec)
+    val redisClusterClientWithLongValue = LettuceRedisCommandsClient(clusterConfig, RedisCodec.Utf8WithValueAsLongCodec)
+    new RedisCommandCachedClients(
+      redisSingleNodeClientWithStringValue,
+      redisSingleNodeClientWithLongValue,
+      redisClusterClientWithStringValue,
+      redisClusterClientWithLongValue
+    )
+  }
+}
